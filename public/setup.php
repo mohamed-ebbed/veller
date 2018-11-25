896a80e15929a7fd0e5175603de613d2a22ffe66
Mohamed Ramzy Helmy <mramzytechioneer@gmail.com>
2018-11-25 20:39:52 +0200

    Finished database setup file

--- public/setup.php
+++ public/setup.php
@@ -3,6 +3,10 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $dbname = "veller";
+
+$conn = new mysqli($host , $user , $password);
+$conn->query("CREATE DATABASE veller");
+$conn->close();
 $conn = new mysqli($host , $user , $password , $dbname);
 
 $sql = "CREATE TABLE User_account(
@@ -70,6 +74,7 @@ $sql = "create table education(
 );";
 
 $conn->query($sql);
+echo $conn->error;
 
 $sql = "CREATE TABLE interests(
     applicant_id INT NOT NULL,
@@ -79,8 +84,143 @@ $sql = "CREATE TABLE interests(
 )";
 
 $conn->query($sql);
+echo $conn->error;
 
 
+$sql = "CREATE TABLE Opportunity (
+	post_id INT NOT NULL,
+	post_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	expiration_date DATE NOT NULL,
+	description TEXT NOT NULL,
+	country VARCHAR(30),
+	city VARCHAR(30),
+	duration VARCHAR(50) NOT NULL,
+	funded VARCHAR(20) NOT NULL,
+	requirements TEXT NOT NULL,
+	posted_by INT NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (posted_by) REFERENCES Organization(id) ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "
+CREATE TABLE Volunteering(
+	post_id	INT NOT NULL,
+	previous_experience TEXT NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Contest(
+	post_id INT NOT NULL,
+	prizes TEXT,
+	specialization VARCHAR(50) NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Internship(
+	post_id INT NOT NULL,
+	specialization VARCHAR(50) NOT NULL,
+	paid BIT NOT NULL DEFAULT 0,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Exchange_Program(
+	post_id INT NOT NULL,
+	specialization VARCHAR(50) NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Scholarship (
+	post_id INT NOT NULL,
+	specialization VARCHAR(50) NOT NULL,
+	paid BIT NOT NULL DEFAULT 0,
+	type VARCHAR(20),
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Tags(
+	post_id INT NOT NULL,
+	tag VARCHAR(20) NOT NULL,
+	PRIMARY KEY(post_id, tag),
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Applicable_Countries (
+	post_id INT NOT NULL,
+	country VARCHAR(30) NOT NULL,
+	PRIMARY KEY (post_id, country),
+	FOREIGN KEY (post_id) REFERENCES Exchange_Program ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
 
 
+$sql = "CREATE TABLE Apply_For (
+	id INT NOT NULL,
+	post_id INT NOT NULL,
+	PRIMARY KEY(id, post_id),
+	FOREIGN KEY (id) REFERENCES Applicant,
+	FOREIGN KEY (post_id) REFERENCES Opportunity ON DELETE CASCADE ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Supervisor(
+	id INT NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	phone_number VARCHAR(15) NOT NULL,
+	country VARCHAR(30) NOT NULL,
+	city VARCHAR(30) NOT NULL,
+	zip INT NOT NULL,
+	password VARCHAR(100) NOT NULL,
+	support_tickets_count INT NOT NULL DEFAULT 0,
+	PRIMARY KEY (id)	
+)";
+$conn->query($sql);
+echo $conn->error;
+
+$sql = "CREATE TABLE Support_Tickets(
+	ticket_id INT NOT NULL,
+	sent_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	content TEXT NOT NULL,
+	solved BIT NOT NULL DEFAULT 0,
+	solved_by INT,
+	sent_by INT,
+	PRIMARY KEY (ticket_id),
+	FOREIGN KEY (solved_by) REFERENCES Supervisor ON DELETE SET NULL ON UPDATE RESTRICT,
+	FOREIGN KEY (sent_by) REFERENCES User_Account ON DELETE SET NULL ON UPDATE RESTRICT
+)";
+
+$conn->query($sql);
+echo $conn->error;
+
+print("Database created successfully");
 ?>
