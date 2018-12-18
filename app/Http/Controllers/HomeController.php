b8b312aacfa6eaa92ecbf87fdcc188abab1b3287
Mohamed Ramzy Helmy <mramzytechioneer@gmail.com>
2018-12-19 00:03:55 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
     public function index()
     {
         $auth = new CustomAuth();
-        return view('home');
+        return view('welcome');
     }
     
 }
